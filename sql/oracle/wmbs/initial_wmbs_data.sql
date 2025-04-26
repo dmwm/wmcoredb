79dc5b53082fbb12a85f9556d0f7179de46520b7
Alan Malta Rodrigues <alan.malta@cern.ch>
2025-04-26 00:26:45 -0400

    fix: data insert for auto-increment wmbs tables

--- sql/oracle/wmbs/initial_wmbs_data.sql
+++ sql/oracle/wmbs/initial_wmbs_data.sql
@@ -1,41 +1,41 @@
 -- Initial data inserts for Oracle
 
 -- Job states, dependent on src/python/WMCore/JobStateMachine/Transitions.py
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'new');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'created');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'executing');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'complete');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'success');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'retrydone');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'exhausted');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'killed');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'createcooloff');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'createfailed');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'createpaused');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'submitcooloff');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'submitfailed');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'submitpaused');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'jobcooloff');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'jobfailed');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'jobpaused');
-INSERT INTO wmbs_job_state (id, name) VALUES (wmbs_job_state_SEQ.nextval, 'cleanout');
+INSERT INTO wmbs_job_state (name) VALUES ('new');
+INSERT INTO wmbs_job_state (name) VALUES ('created');
+INSERT INTO wmbs_job_state (name) VALUES ('executing');
+INSERT INTO wmbs_job_state (name) VALUES ('complete');
+INSERT INTO wmbs_job_state (name) VALUES ('success');
+INSERT INTO wmbs_job_state (name) VALUES ('retrydone');
+INSERT INTO wmbs_job_state (name) VALUES ('exhausted');
+INSERT INTO wmbs_job_state (name) VALUES ('killed');
+INSERT INTO wmbs_job_state (name) VALUES ('createcooloff');
+INSERT INTO wmbs_job_state (name) VALUES ('createfailed');
+INSERT INTO wmbs_job_state (name) VALUES ('createpaused');
+INSERT INTO wmbs_job_state (name) VALUES ('submitcooloff');
+INSERT INTO wmbs_job_state (name) VALUES ('submitfailed');
+INSERT INTO wmbs_job_state (name) VALUES ('submitpaused');
+INSERT INTO wmbs_job_state (name) VALUES ('jobcooloff');
+INSERT INTO wmbs_job_state (name) VALUES ('jobfailed');
+INSERT INTO wmbs_job_state (name) VALUES ('jobpaused');
+INSERT INTO wmbs_job_state (name) VALUES ('cleanout');
 
 -- Subscription types
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Production', 0);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Processing', 0);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Cleanup', 1);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'LogCollect', 2);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Skim', 3);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Merge', 4);
-INSERT INTO wmbs_sub_types (id, name, priority) VALUES (wmbs_sub_types_SEQ.nextval, 'Harvesting', 5);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Production', 0);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Processing', 0);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Cleanup', 1);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('LogCollect', 2);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Skim', 3);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Merge', 4);
+INSERT INTO wmbs_sub_types (name, priority) VALUES ('Harvesting', 5);
 
 -- Location states
-INSERT INTO wmbs_location_state (id, name) VALUES (wmbs_location_state_SEQ.nextval, 'Normal');
-INSERT INTO wmbs_location_state (id, name) VALUES (wmbs_location_state_SEQ.nextval, 'Down');
-INSERT INTO wmbs_location_state (id, name) VALUES (wmbs_location_state_SEQ.nextval, 'Draining');
-INSERT INTO wmbs_location_state (id, name) VALUES (wmbs_location_state_SEQ.nextval, 'Aborted');
+INSERT INTO wmbs_location_state (name) VALUES ('Normal');
+INSERT INTO wmbs_location_state (name) VALUES ('Down');
+INSERT INTO wmbs_location_state (name) VALUES ('Draining');
+INSERT INTO wmbs_location_state (name) VALUES ('Aborted');
 
 -- Checksum types
-INSERT INTO wmbs_checksum_type (id, type) VALUES (wmbs_checksum_type_SEQ.nextval, 'cksum');
-INSERT INTO wmbs_checksum_type (id, type) VALUES (wmbs_checksum_type_SEQ.nextval, 'adler32');
-INSERT INTO wmbs_checksum_type (id, type) VALUES (wmbs_checksum_type_SEQ.nextval, 'md5'); 
+INSERT INTO wmbs_checksum_type (type) VALUES ('cksum');
+INSERT INTO wmbs_checksum_type (type) VALUES ('adler32');
+INSERT INTO wmbs_checksum_type (type) VALUES ('md5');
